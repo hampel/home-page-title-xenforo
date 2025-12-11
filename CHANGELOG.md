bc7dfb9d8807ba524228dddc23c37628aa8995c8
Simon Hampel <simon@hampelgroup.com>
2025-12-11 16:59:34 +1100

    bump version to 2.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 CHANGELOG
 =========
 
+2.0.2 (2025-12-11)
+------------------
+
+* run enqueuePostUpgradeCleanUp during upgrades if we're running XF2.3+
+
 2.0.1 (2024-07-12)
 ------------------
 
---
+++ _output/option_hint.php
@@ -0,0 +1,19 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property string|null $hampelHomePageTitle Home Page Title
+ * @property bool|null $hampelHomePageTitleReplaceMetadata Replace metadata
+ */
+class Options
+{
+}
